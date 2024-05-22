129fa488680a5c0dea4c82c1681abb3abd299431
Samir <sobiroff23@gmail.com>
2024-05-22 22:27:49 +0500

    fixed

--- app/(yoom)/recordings/page.tsx
+++ app/(yoom)/recordings/page.tsx
@@ -1,11 +1,23 @@
 import { Button } from "@/components/ui/button";
 
-
-
 export default function Home() {
-    return (
-        <div>
-            <h1>Recorings</h1>
+  return (
+    <>
+      <div className="items-center pl-[20%]">
+        <h1 className="text-start  pt-[47px] text-white font-bold text-3xl">
+          Meeting Recordings
+        </h1>
+
+        <div className=" w-full grid grid-cols-2 gap-2">
+          <div className="w-[500px] h-[250px] bg-darkblue rounded-lg"></div>
+
+          <div className="w-[500px] h-[250px] bg-darkblue rounded-lg"></div>
+
+          <div className="w-[500px] h-[250px] bg-darkblue rounded-lg"></div>
+
+          <div className="w-[500px] h-[250px] bg-darkblue rounded-lg"></div>
         </div>
-    )
+      </div>
+    </>
+  );
 }
