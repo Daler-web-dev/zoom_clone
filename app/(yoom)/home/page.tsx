ad11163f989196f87914604fb9df0f3a5aa405db
GradusFrontend <ruslandjavadov998@gmail.com>
2024-05-23 01:18:21 +0500

    meeting component

--- app/(yoom)/home/page.tsx
+++ app/(yoom)/home/page.tsx
@@ -1,11 +1,15 @@
+import MeetingCard from "@/components/Meeting";
 import { Button } from "@/components/ui/button";
 
 
 
 export default function Home() {
     return (
-        <div>
-            <h1>HOME</h1>
+        <div className="grid grid-cols-2 gap-3">
+           <MeetingCard title="FHFjf sjdfnsjfnsjfn sdfsf" date="2220 2323 f f3 33" users={[{}]} isPassed={false}></MeetingCard>
+           <MeetingCard title="FHFjf sjdfnsjfnsjfn sdfsf" date="2220 2323 f f3 33" users={[{}]} isPassed={false}></MeetingCard>
+           <MeetingCard title="FHFjf sjdfnsjfnsjfn sdfsf" date="2220 2323 f f3 33" users={[{}]} isPassed={true}></MeetingCard>
+           <MeetingCard title="FHFjf sjdfnsjfnsjfn sdfsf" date="2220 2323 f f3 33" users={[{}]} isPassed={false}></MeetingCard>
         </div>
     )
 }
