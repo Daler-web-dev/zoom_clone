e48a49882e4db30173960952d9bd05021a969d88
GradusFrontend <ruslandjavadov998@gmail.com>
2024-05-24 14:32:50 +0500

    mini fix

--- app/(yoom)/home/page.tsx
+++ app/(yoom)/home/page.tsx
@@ -5,11 +5,6 @@ import { Button } from "@/components/ui/button";
 
 export default function Home() {
     return (
-        <div className="grid grid-cols-2 gap-3">
-           <MeetingCard title="FHFjf sjdfnsjfnsjfn sdfsf" date="2220 2323 f f3 33" users={[{}]} isPassed={false}></MeetingCard>
-           <MeetingCard title="FHFjf sjdfnsjfnsjfn sdfsf" date="2220 2323 f f3 33" users={[{}]} isPassed={false}></MeetingCard>
-           <MeetingCard title="FHFjf sjdfnsjfnsjfn sdfsf" date="2220 2323 f f3 33" users={[{}]} isPassed={true}></MeetingCard>
-           <MeetingCard title="FHFjf sjdfnsjfnsjfn sdfsf" date="2220 2323 f f3 33" users={[{}]} isPassed={false}></MeetingCard>
-        </div>
+       <></>
     )
 }
