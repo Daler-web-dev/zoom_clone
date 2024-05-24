66ba4b6f8fd594965a527a5483d1b711b8947d0a
Daler-web-dev <dsarifkulo021@gmail.com>
2024-05-24 14:43:23 +0500

    fixed

--- components/SettingBlocks.tsx
+++ components/SettingBlocks.tsx
@@ -11,7 +11,12 @@ type IBlock = {
 
  const  SettingBlocks: React.FC<IBlock> = ({src, title, desc, bg}) => {
     return (
-        <div className={`w-[260px] h-[260px] rounded-[14px] bg-[${bg}] flex justify-between flex-col items-start py-6 px-5 cursor-pointer`}>
+        <div 
+            className={`w-[260px] h-[260px] rounded-[14px] flex justify-between flex-col items-start py-6 px-5 cursor-pointer`}
+            style={{
+                background: bg
+            }}
+        >
             <div className="w-[56px] h-[56px] rounded-[10px] bg-white/35 flex justify-center items-center">
                 <img className="w-[36px] h-[36px]" src={src} alt="icons" />
             </div>
