1bdc3d7de4298b500d4e09e4ff7aef8ccba72d8a
Samir <sobiroff23@gmail.com>
2024-05-27 13:55:27 +0500

    fixed yoom regist

--- app/(yoom)/recordings/page.tsx
+++ app/(yoom)/recordings/page.tsx
@@ -1,23 +1,19 @@
 import Recordings from "@/components/Recordings";
 
-
-
-
 export default function Home() {
   return (
     <>
-    <div className="items-center pl-[20%]">
-      <h1 className="text-start  pt-[47px] text-white font-bold text-3xl">
-      Meeting Recordings
-      </h1>
-      <div className="grid grid-cols-2 gap-6 pt-7">
-    <Recordings title="#f23cv1d" beginning="1hr 33mins" ending="33mins" />
-    <Recordings title="#f23cv1d" beginning="1hr 33mins" ending="33mins" />
-    <Recordings title="#f23cv1d" beginning="1hr 33mins" ending="33mins" />
-    <Recordings title="#f23cv1d" beginning="1hr 33mins" ending="33mins" />
-
-    </div>
-    </div>
+      <div className="items-center pl-[20%]">
+        <h1 className="text-start  pt-[47px] text-white font-bold text-3xl">
+          Meeting Recordings
+        </h1>
+        <div className="grid grid-cols-2 gap-6 pt-7">
+          <Recordings title="#f23cv1d" beginning="1hr 33mins" ending="33mins" />
+          <Recordings title="#f23cv1d" beginning="1hr 33mins" ending="33mins" />
+          <Recordings title="#f23cv1d" beginning="1hr 33mins" ending="33mins" />
+          <Recordings title="#f23cv1d" beginning="1hr 33mins" ending="33mins" />
+        </div>
+      </div>
     </>
   );
 }
