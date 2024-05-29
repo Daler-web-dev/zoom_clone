a4ce5e5ca55e2a6df92a971a28ff0b30abffcfe7
Samir <sobiroff23@gmail.com>
2024-05-29 14:10:58 +0500

    fixed modals

--- app/(yoom)/(home)/page.tsx
+++ app/(yoom)/(home)/page.tsx
@@ -1,38 +1,88 @@
+"use client";
 import ModalDialog from "@/components/ModalDialog";
 import SettingBlocks from "@/components/SettingBlocks";
 import TimeComponent from "@/components/Time";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-
-
+import React, { useState } from "react";
 
 export default function Home() {
-    return (
-        <div className="pt-[30px] pr-[25px] ">
-            <TimeComponent />
-            <div className="py-8 flex justify-start items-center gap-4 ">
-                <SettingBlocks bg="#FF742E" desc="Setup a new recording" title="New Meeting" src="/icons/Meeting.svg" />
-                <SettingBlocks bg="#0E78F9" desc="via invitation link" title="Join Meeting" src="/icons/Join.svg" />
-                <SettingBlocks bg="#830EF9" desc="Plan your meeting" title="Schedule Meeting" src="/icons/Schedule.svg" />
-                <SettingBlocks bg="#F9A90E" desc="Meeting recordings" title="View Recordings" src="/icons/View.svg" />
-            </div>
-            <ModalDialog 
-                title={"Create Meeting"}
+  const [isFirstModalOpen, setIsFirstModalOpen] = useState(true);
+  const [isSecondModalOpen, setIsSecondModalOpen] = useState(false);
+
+  const handleCreateMeeting = () => {
+    setIsFirstModalOpen(false);
+    setIsSecondModalOpen(true);
+  };
+
+  return (
+    <div className="pt-[30px] pr-[25px] ">
+      <TimeComponent />
+      <div className="py-8 flex justify-start items-center gap-4 ">
+        <SettingBlocks
+          bg="#FF742E"
+          desc="Setup a new recording"
+          title="New Meeting"
+          src="/icons/Meeting.svg"
+        />
+        <SettingBlocks
+          bg="#0E78F9"
+          desc="via invitation link"
+          title="Join Meeting"
+          src="/icons/Join.svg"
+        />
+        <SettingBlocks
+          bg="#830EF9"
+          desc="Plan your meeting"
+          title="Schedule Meeting"
+          src="/icons/Schedule.svg"
+        />
+        <SettingBlocks
+          bg="#F9A90E"
+          desc="Meeting recordings"
+          title="View Recordings"
+          src="/icons/View.svg"
+        />
+      </div>
+      {isFirstModalOpen && (
+        <ModalDialog title={"Create Meeting"}>
+          <div className="pt-6">
+            <span>Add a description</span>
+            <Input />
+            <span>Select Date & Time</span>
+            <Input />
+            <Button
+              className="w-full mt-5"
+              onClick={() => {
+                handleCreateMeeting();
+              }}
             >
-                <div className="pt-6">
-                <span>Add a description</span>
-                <Input />
-                <span className="">Select Date & Time</span>
-                <Input />
-                <Button className="w-full mt-5">Create Meeting</Button>
-                </div>
-            </ModalDialog>
+              Create Meeting
+            </Button>
+          </div>
+        </ModalDialog>
+      )}
 
+      {isSecondModalOpen && (
+        <ModalDialog title={"Meeting Created"}>
+          <div className="pt-6">
+            <span> </span>
+            <Button
+              className="w-full mt-5 bg-[#0E78F9]"
+            >
+              Copy Invitation
+            </Button>
 
+            <Button
+              className="w-full mt-5"
+              onClick={() => setIsSecondModalOpen(false)}
             
-        </div>
-
-
-
-    )
+            >
+              Close
+            </Button>
+          </div>
+        </ModalDialog>
+      )}
+    </div>
+  );
 }
