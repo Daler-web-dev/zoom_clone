6ed8c5e3c213cd88d921bc972b7fc9fd6f2b9af8
Daler-web-dev <dsarifkulo021@gmail.com>
2024-05-29 16:05:55 +0500

    fixed

--- components/ModalDialog.tsx
+++ components/ModalDialog.tsx
@@ -1,22 +1,58 @@
 import {
     Dialog,
+    DialogClose,
     DialogContent,
     DialogDescription,
     DialogHeader,
     DialogTitle,
     DialogTrigger,
 } from "@/components/ui/dialog"
+import Image from "next/image";
 import { ReactNode } from "react"
+import { Button } from "./ui/button";
+import { LuCopy } from "react-icons/lu";
+import { IoClose } from "react-icons/io5";
+interface MeetingModalProps {
+    isOpen: boolean;
+    onClose: () => void;
+    title: string;
+    className?: string;
+    children?: ReactNode;
+    handleClick?: () => void;
+    buttonText?: string;
+    instantMeeting?: boolean;
+    image?: string;
+    buttonClassName?: string;
+    buttonIcon?: boolean;
+}
 
-export default function ModalDialog({title, children}: {title: string, children: ReactNode }) {
+
+export default function ModalDialog({ title, image, children, className, buttonText, buttonIcon = false }: MeetingModalProps) {
     return (
         <Dialog>
-            <DialogTrigger>Open</DialogTrigger>
-            <DialogContent>
-                <DialogHeader>
+            <DialogTrigger className="text-white" >Open</DialogTrigger>
+            <DialogContent
+                className="bg-darkblue w-full border-none text-white"
+            >
+                <DialogHeader
+                    className="flex items-center justify-center gap-2 flex-col"
+                >
+                    {image && <Image src={image} alt={title} width={200} height={200} className="w-20 h-20" />}
                     <DialogTitle>{title}</DialogTitle>
-                    {children}
                 </DialogHeader>
+                {children}
+                {/* <Button 
+                    variant="default" 
+                    className="flex items-center gap-2 bg-[#0E78F9] text-white" 
+                > 
+                   <LuCopy/>
+                </Button> */}
+
+                <DialogClose asChild>
+                    <Button type="button" variant="default">
+                        Close
+                    </Button>
+                </DialogClose>
             </DialogContent>
         </Dialog>
     )
