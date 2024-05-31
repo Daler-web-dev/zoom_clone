7ae9a624da1f032239986320a1f7d5346f61ce70
MuniS67 <tursunovmunis34@gmail.com>
2024-05-31 11:36:22 +0500

    changed

--- app/(yoom)/(home)/page.tsx
+++ app/(yoom)/(home)/page.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import ModalDialog from "@/components/ModalDialog";
 import SettingBlocks from "@/components/SettingBlocks";
@@ -11,61 +11,103 @@ import DatePicker from "react-datepicker";
 import { useForm } from "react-hook-form";
 import { LuCopy } from "react-icons/lu";
 
-
 export default function Home() {
-    const {
-        register,
-        handleSubmit,
-        formState: { errors },
-    } = useForm();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
 
-    const onSubmit = async (data: any) => {
-        console.log(data);
-    }
+  const onSubmit = async (data: any) => {
+    console.log(data);
 
-    return (
-        <div>
-            <TimeComponent />
-            <div className="py-8 flex justify-start items-center gap-4">
-                <button
-                >
-                    <SettingBlocks bg="#FF742E" desc="Setup a new recording" title="New Meeting" src="/icons/Meeting.svg" />
-                </button>
-                <button>
-                    <SettingBlocks bg="#0E78F9" desc="via invitation link" title="Join Meeting" src="/icons/Join.svg" />
-                </button>
-                <button>
-                    <SettingBlocks bg="#830EF9" desc="Plan your meeting" title="Schedule Meeting" src="/icons/Schedule.svg" />
-                </button>
-                <button>
-                    <SettingBlocks bg="#F9A90E" desc="Meeting recordings" title="View Recordings" src="/icons/View.svg" />
-                </button>
-            </div>
-            <ModalDialog
-                isOpen={false}
-                onClose={() => false}
-                buttonText="Create Meeting"
-                title={"Create Meeting"}
-            >
-                <form onSubmit={handleSubmit(onSubmit)} >
-                    <Textarea
-                        {...register('description', { required: true })}
-                    />
-                    <Input
-                        type="date"
-                        className="text-black"
-                        {...register('date', { required: true })}
-                    />
-                    <Button
-                        variant="default"
-                        className="flex items-center gap-2 bg-[#0E78F9] text-white"
-                        type="submit"
-                    >
-                        Create Meeting
-                    </Button>
-                </form>
-            </ModalDialog>
+    // data.length > 0 ? (
+    //     <ModalDialog
+    //         isOpen={false}
+    //         onClose={() => false}
+    //         buttonText="Meeting Created"
+    //         title={"Meeting Created"}
+    //         image="/icons/verified.svg"
+    //     >
+    //     <Button>
+    //         <LuCopy /> Copy Invitation
+    //     </Button>
+    //     </ModalDialog>
+    // ) : null;
+  };
 
-        </div>
-    )
+  return (
+    <div>
+      <TimeComponent />
+      <div className="py-8 flex justify-start items-center gap-4">
+        <button>
+          <SettingBlocks
+            bg="#FF742E"
+            desc="Setup a new recording"
+            title="New Meeting"
+            src="/icons/Meeting.svg"
+          />
+        </button>
+        <button>
+          <SettingBlocks
+            bg="#0E78F9"
+            desc="via invitation link"
+            title="Join Meeting"
+            src="/icons/Join.svg"
+          />
+        </button>
+        <button>
+          <SettingBlocks
+            bg="#830EF9"
+            desc="Plan your meeting"
+            title="Schedule Meeting"
+            src="/icons/Schedule.svg"
+          />
+        </button>
+        <button>
+          <SettingBlocks
+            bg="#F9A90E"
+            desc="Meeting recordings"
+            title="View Recordings"
+            src="/icons/View.svg"
+          />
+        </button>
+      </div>
+      <ModalDialog
+        isOpen={false}
+        onClose={() => false}
+        buttonText="Create Meeting"
+        title={"Create Meeting"}
+      >
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className="flex flex-col items-start justify-start">
+            <label className="text-base font-normal" htmlFor="description">
+              Add a description
+            </label>
+            <Textarea
+              className="font-normal text-base text-black"
+              {...register("description", { required: true })}
+            />
+          </div>
+          <div className="flex flex-col items-start justify-start py-2.5">
+            <label className="text-base font-normal" htmlFor="date">
+              Select Date & Time
+            </label>
+            <Input
+              type="date"
+              className="text-black font-normal text-base"
+              {...register("date", { required: true })}
+            />
+          </div>
+          <Button
+            variant="default"
+            className="w-full flex items-center gap-2 bg-[#0E78F9] text-white"
+            type="submit"
+          >
+            Create Meeting
+          </Button>
+        </form>
+      </ModalDialog>
+    </div>
+  );
 }
--- components/ModalDialog.tsx
+++ components/ModalDialog.tsx
@@ -1,59 +1,64 @@
 import {
-    Dialog,
-    DialogClose,
-    DialogContent,
-    DialogDescription,
-    DialogHeader,
-    DialogTitle,
-    DialogTrigger,
-} from "@/components/ui/dialog"
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
 import Image from "next/image";
-import { ReactNode } from "react"
+import { ReactNode } from "react";
 import { Button } from "./ui/button";
 import { LuCopy } from "react-icons/lu";
 import { IoClose } from "react-icons/io5";
 interface MeetingModalProps {
-    isOpen: boolean;
-    onClose: () => void;
-    title: string;
-    className?: string;
-    children?: ReactNode;
-    handleClick?: () => void;
-    buttonText?: string;
-    instantMeeting?: boolean;
-    image?: string;
-    buttonClassName?: string;
-    buttonIcon?: boolean;
+  isOpen: boolean;
+  onClose: () => void;
+  title: string;
+  className?: string;
+  children?: ReactNode;
+  handleClick?: () => void;
+  buttonText?: string;
+  instantMeeting?: boolean;
+  image?: string;
+  buttonClassName?: string;
+  buttonIcon?: boolean;
 }
 
-
-export default function ModalDialog({ title, image, children, className, buttonText, buttonIcon = false }: MeetingModalProps) {
-    return (
-        <Dialog>
-            <DialogTrigger className="text-white" >Open</DialogTrigger>
-            <DialogContent
-                className="bg-darkblue w-full border-none text-white"
-            >
-                <DialogHeader
-                    className="flex items-center justify-center gap-2 flex-col"
-                >
-                    {image && <Image src={image} alt={title} width={200} height={200} className="w-20 h-20" />}
-                    <DialogTitle>{title}</DialogTitle>
-                </DialogHeader>
-                {children}
-                {/* <Button 
-                    variant="default" 
-                    className="flex items-center gap-2 bg-[#0E78F9] text-white" 
-                > 
-                   <LuCopy/>
-                </Button> */}
-
-                <DialogClose asChild>
-                    <Button type="button" variant="default">
-                        Close
-                    </Button>
-                </DialogClose>
-            </DialogContent>
-        </Dialog>
-    )
+export default function ModalDialog({
+  title,
+  image,
+  children,
+  className,
+  buttonText,
+  buttonIcon = false,
+}: MeetingModalProps) {
+  return (
+    <Dialog>
+      {title !== "Meeting Created" ? <DialogTrigger className="text-white">Open</DialogTrigger> : <DialogTrigger className="text-white" aria-expanded="true" data-state="open">Open</DialogTrigger>}
+      <DialogContent className="bg-darkblue w-full border-none text-white">
+        <DialogHeader className="flex items-center justify-center gap-2 flex-col">
+          {image && (
+            <Image
+              src={image}
+              alt={title}
+              width={200}
+              height={200}
+              className="w-20 h-20"
+            />
+          )}
+          <DialogTitle>{title}</DialogTitle>
+        </DialogHeader>
+        {children}
+        {title === "Meeting Created" && (
+          <DialogClose asChild>
+            <Button type="button" variant="default">
+              Close
+            </Button>
+          </DialogClose>
+        )}
+      </DialogContent>
+    </Dialog>
+  );
 }
