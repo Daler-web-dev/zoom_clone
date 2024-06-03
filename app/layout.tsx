43ac1fcd890b0ff62ed24e0082a02cef988559f7
MuniS97 <tursunovmunis141@gmail.com>
2024-06-03 13:56:36 +0500

    added toast

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+import { Toast } from "@/components/ui/toast";
+import { Toaster } from "@/components/ui/toaster";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -32,7 +34,10 @@ export default function RootLayout({
 					}
 				}}
 			>
-				<body className={`${inter.className} bg-main `}>{children}</body>
+				<body className={`${inter.className} bg-main `}>
+					{children}
+					<Toaster/>
+				</body>
 			</ClerkProvider>
 		</html>
 	);
