dc4f156f6a6bb63ccec1d27af4dec0c53d7bf9fb
MuniS97 <tursunovmunis141@gmail.com>
2024-06-03 14:31:23 +0500

    worked on meeting pages

--- app/(yoom)/meeting/[id]/page.tsx
+++ app/(yoom)/meeting/[id]/page.tsx
@@ -1,16 +1,33 @@
 'use client'
 
+import MeetingRoom from '@/components/MeetingRoom'
+import MeetingSetup from '@/components/MeetingSetup'
+import { useGetcallById } from '@/hooks/useGetCallbyId'
 import { useUser } from '@clerk/nextjs'
+import { StreamCall, StreamTheme } from '@stream-io/video-react-sdk'
+import { Loader } from 'lucide-react'
 import { useParams } from 'next/navigation'
 import React, { useState } from 'react'
 
 export default function Meeting() {
-	const {id} = useParams()
-	const {user, isLoaded} = useUser()
-	const [] = useState()
+	const { id } = useParams()
+	const { user, isLoaded } = useUser()
+	const [isSetupComplete, setIsSetupComplete] = useState(false)
+	const { call, isCallLoading } = useGetcallById(id)
 
-	
+	if(!isLoaded || isCallLoading) return <Loader />
+
 	return (
-		<div>page</div>
+		<main className='h-screen w-full'>
+			<StreamCall call={call}>
+				<StreamTheme>
+					{!isSetupComplete ? (
+						<MeetingSetup />
+					) : (
+						<MeetingRoom />
+					)}
+				</StreamTheme>
+			</StreamCall>
+		</main>
 	)
 }
