b3f16b597e3440c00e306012b1257c8b2528cae8
Daler-web-dev <dsarifkulo021@gmail.com>
2024-06-05 15:01:18 +0500

    fixed

--- app/(yoom)/meeting/[id]/page.tsx
+++ app/(yoom)/meeting/[id]/page.tsx
@@ -1,23 +1,29 @@
 'use client'
+import MeetingRoom from '@/components/MeetingRoom'
 import SetupPreview from '@/components/SetupPreview'
 import { useGetCallById } from '@/hooks/useGetCallById'
 import { useUser } from '@clerk/nextjs'
 import { Call, StreamCall, StreamTheme, VideoPreview, useCall, useCallStateHooks } from '@stream-io/video-react-sdk'
 import { useParams } from 'next/navigation'
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function Meeting() {
+	const [setCompleted, setSetUpCompleted] = useState<boolean>(false)
 	const { id } = useParams()
-	
-	const { user, isLoaded } = useUser()
-	const {call, isCallLoading} = useGetCallById(id)
 
-	if(!isLoaded || isCallLoading) return <h1>Loading...</h1>
+	const { user, isLoaded } = useUser()
+	const { call, isCallLoading } = useGetCallById(id)
+
+	if (!isLoaded || isCallLoading) return <h1>Loading...</h1>
 
 	return (
 		<StreamCall call={call}>
 			<StreamTheme>
-				<SetupPreview/>
+				{
+					!setCompleted ?
+						<SetupPreview setSetUpCompleted={setSetUpCompleted} /> :
+						<MeetingRoom />
+				}
 			</StreamTheme>
 		</StreamCall>
 	)
--- components/SetupPreview.tsx
+++ components/SetupPreview.tsx
@@ -2,14 +2,14 @@
 import { DeviceSettings, VideoPreview, useCall } from '@stream-io/video-react-sdk'
 import React, { useEffect, useState } from 'react'
 
-export default function SetupPreview() {
+export default function SetupPreview({ setSetUpCompleted }: { setSetUpCompleted: (arg: boolean) => void }) {
 	const [toggle, setToggle] = useState(false)
 	const call = useCall()
 
-	if(!call) return
+	if (!call) return
 
 	useEffect(() => {
-		if(toggle) {
+		if (toggle) {
 			call.camera.enable()
 			call.microphone.enable()
 		} else {
@@ -24,19 +24,30 @@ export default function SetupPreview() {
 		>
 			<div>
 				<h1 className='font-bold text-2xl' >Setup</h1>
-				<VideoPreview />
-				<label 
+				<div className='w-[600px] h-[400px] border-blue-800 border-2 flex items-center justify-center ' >
+					<VideoPreview />
+				</div>
+				<label
 					className='flex items-center gap-2 '
 					htmlFor="micCam"
 				>
-					<input 
-						type="checkbox" 
+					<input
+						type="checkbox"
 						id='micCam'
 						onChange={(e) => setToggle(e.target.checked)}
 					/>
 					Join with camera and microphone
 				</label>
-				<DeviceSettings/>
+				<DeviceSettings />
+				<button
+					onClick={() => { 
+						call.join()
+
+						setSetUpCompleted(true)
+					}}
+				>
+					join meeting
+				</button>
 			</div>
 		</section>
 	)
