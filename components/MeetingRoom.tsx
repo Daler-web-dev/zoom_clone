6e3edbecbfe0ffd573b366756a4daa9b963a70d2
Daler-web-dev <dsarifkulo021@gmail.com>
2024-06-05 15:13:51 +0500

    fixed

--- components/MeetingRoom.tsx
+++ components/MeetingRoom.tsx
@@ -1,7 +1,21 @@
+"use client"
+import { CallControls, CallParticipantsList, CallStatsButton, CallingState, PaginatedGridLayout, SpeakerLayout } from '@stream-io/video-react-sdk'
+import { useRouter } from 'next/navigation'
 import React from 'react'
 
+
 export default function MeetingRoom() {
-  return (
-    <div>MeetingRoom</div>
-  )
+    const route = useRouter()
+
+    return (
+        <section
+            className='flex items-center justify-center w-full h-screen text-center text-white'
+        >
+            <CallControls/>
+            <CallParticipantsList onClose={() => route.push('/')} />
+            {/* <SpeakerLayout participantsBarPosition="left" /> */}
+            <PaginatedGridLayout/>
+            <CallStatsButton/>
+        </section>
+    )
 }
