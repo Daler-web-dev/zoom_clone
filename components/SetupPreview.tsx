ac2e2acc0f25d3559a5dab7168b9f55ef445c39d
saloxitdinovv <saloxitdinovv@gmail.com>
2024-06-07 13:20:03 +0500

    enter video chat fixed

--- components/SetupPreview.tsx
+++ components/SetupPreview.tsx
@@ -2,8 +2,10 @@
 import { DeviceSettings, VideoPreview, useCall } from '@stream-io/video-react-sdk'
 import React, { useEffect, useState } from 'react'
 
+
 export default function SetupPreview({ setSetUpCompleted }: { setSetUpCompleted: (arg: boolean) => void }) {
 	const [toggle, setToggle] = useState(false)
+
 	const call = useCall()
 
 	if (!call) {
@@ -24,32 +26,39 @@ export default function SetupPreview({ setSetUpCompleted }: { setSetUpCompleted:
 		<section
 			className='flex items-center justify-center w-full h-screen text-center text-white'
 		>
-			<div>
-				<h1 className='font-bold text-2xl' >Setup</h1>
-				<div className='w-[600px] h-[400px] border-blue-800 border-2 flex items-center justify-center ' >
-					<VideoPreview />
+			<div className='flex gap-14'>
+				{/* <h1 className='font-bold text-2xl' >Setup</h1> */}
+				<div>
+					<div className='w-[600px] h-[400px] border-blue-800 border-2 flex items-center justify-center ' >
+						<VideoPreview />
+					</div>
+					<label
+						className='flex items-center gap-2 '
+						htmlFor="micCam"
+					>
+						<input
+							type="checkbox"
+							id='micCam'
+							onChange={(e) => setToggle(e.target.checked)}
+						/>
+						Join with camera and microphone
+					</label>
+					<DeviceSettings />
 				</div>
-				<label
-					className='flex items-center gap-2 '
-					htmlFor="micCam"
-				>
-					<input
-						type="checkbox"
-						id='micCam'
-						onChange={(e) => setToggle(e.target.checked)}
-					/>
-					Join with camera and microphone
-				</label>
-				<DeviceSettings />
-				<button
-					onClick={() => { 
-						call.join()
 
-						setSetUpCompleted(true)
-					}}
-				>
-					join meeting
-				</button>
+				<div className='flex flex-col gap-7 items-center justify-center'>
+					<h1 className='font-bold text-3xl'>Ready to Join ?</h1>
+					<button
+						onClick={() => {
+							call.join()
+
+							setSetUpCompleted(true)
+						}}
+						className='py-[15px] px-[40px] bg-[#2684FC] rounded-[10px]'
+					>
+						JOIN
+					</button>
+				</div>
 			</div>
 		</section>
 	)
