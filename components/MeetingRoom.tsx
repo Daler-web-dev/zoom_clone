26122e4430e47cb0c9f5f314039599b416c88e55
saloxitdinovv <saloxitdinovv@gmail.com>
2024-06-07 13:02:44 +0500

    video chat styled

--- components/MeetingRoom.tsx
+++ components/MeetingRoom.tsx
@@ -12,18 +12,19 @@ export default function MeetingRoom() {
             className='flex justify-center w-full h-screen text-center text-white'
         >
             <CallControls />
-            <section className='flex w-screen'>
-                <aside className='bg-green-500 left-0 w-[20%] p-10'>
+            <section className='flex flex-row-reverse w-screen'>
+                <aside className='right-0 w-[20% flex flex-col gap-5 p-10'>
+                    <div className="aside_box h-[100%] bg-[#484747] p-10 rounded-[20px]">
                     <CallParticipantsList onClose={() => route.push('/')} />
+                    </div>
                 </aside>
 
-                <div className="screens bg-yellow-400 w-[80%] p-10">
-                    <PaginatedGridLayout />
+                <div className="screens w-[80%] p-10 flex">
+                    <PaginatedGridLayout/>
+                    {/* <SpeakerLayout participantsBarPosition="left" /> */}
                 </div>
             </section>
             <CallStatsButton />
         </section>
     )
 }
-
-{/* <SpeakerLayout participantsBarPosition="left" /> */}
